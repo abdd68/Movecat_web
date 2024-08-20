ccf0a47d2e867689c43129327c54293b84734a47
y7000p <btian1@umd.edu>
2024-08-19 20:40:11 -0400

    yes

--- pipe.bash
+++ pipe.bash
@@ -7,4 +7,7 @@ heroku ps:scale web=1
 heroku open
 
 # database:redis
+heroku addons:create heroku-redis:mini --app lymph-detection
 heroku redis:cli
+
+heroku addons:destroy redis-dimensional-79467 --app lymph-detection --confirm lymph-detection
